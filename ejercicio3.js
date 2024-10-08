7229ed82bb35bab54ee501a718897adabec69c93
Luinespic <lucia_49_92@hotmail.com>
2024-10-08 18:35:08 +0200

    exercise 3

--- ejercicio3.js
+++ ejercicio3.js
@@ -8,6 +8,9 @@ const users = [
   { id: 4, name: "Amanda" },
 ];
 
+const names = users.map((user) => user.name);
+console.log(names);
+
 /* 3.2 Dado el siguiente array, devuelve una lista que contenga los valores 
 de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
 empiece por 'A'. */
@@ -19,12 +22,29 @@ const users2 = [
   { id: 4, name: "Amanda" },
 ];
 
+const names2 = users2.map((user) => {
+  if (user.name[0] === "A") {
+    return "Anacleto";
+  } else return user.name;
+});
+
+console.log(names2);
+
 /* 3.3 Dado el siguiente array, devuelve una lista que contenga los valores 
 de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
 cuando el valor de la propiedad isVisited = true. */
+
 const cities = [
   { isVisited: true, name: "Tokyo" },
   { isVisited: false, name: "Madagascar" },
   { isVisited: true, name: "Amsterdam" },
   { isVisited: false, name: "Seul" },
 ];
+
+const citiesUpdated = cities.map((city) => {
+  if (city.isVisited) {
+    return city.name + " (Visitado)";
+  } else return city.name;
+});
+
+console.log(citiesUpdated);
